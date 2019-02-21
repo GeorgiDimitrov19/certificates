ac6cf154ac15cf58695ba84656971c813bf09b86
GeorgiDImitrov <46896099+GeorgiDimitrov19@users.noreply.github.com>
2019-02-21 10:29:04 +0200

    Update new-war.groovy

--- new-war.groovy
+++ new-war.groovy
@@ -1,10 +1,15 @@
 node {
    stage('Preparation') { 
-       checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/GeorgiDimitrov19/sampleApplication']]])
+       checkout([$class: 'GitSCM',
+                 branches: [[name: '*/master']],
+                 doGenerateSubmoduleConfigurations: false,
+                 extensions: [],
+                 submoduleCfg: [],
+                 userRemoteConfigs: [[url: 'https://github.com/GeorgiDimitrov19/sampleApplication']]])
    }
    stage('Build') {
-     sh label: '', returnStdout: true, script: 'ansible-playbook -u vagrant /var/lib/jenkins/workspace/MyPipeline6/download-war.yml'
-    
+     sh label: '',
+        returnStdout: true,
+        script: 'ansible-playbook -u vagrant /var/lib/jenkins/workspace/MyPipeline6/download-war.yml'
    }
-   
 }
