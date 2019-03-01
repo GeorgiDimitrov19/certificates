aa5902bee84db4be0fe4b8e9364ecb2ac3a8ddd8
GeorgiDImitrov <46896099+GeorgiDimitrov19@users.noreply.github.com>
2019-03-01 11:53:04 +0200

    Update main-pipeline.groovy

--- main-pipeline.groovy
+++ main-pipeline.groovy
@@ -8,12 +8,14 @@ node {
       userRemoteConfigs: [[url: 'https://github.com/GeorgiDimitrov19/sampleApplication']]])
    }
    stage('Build') {
-     withCredentials([sshUserPrivateKey(credentialsId: 'jenkins-key', keyFileVariable: '', passphraseVariable: '', usernameVariable: '')]) {
-      sh label: '',
-     returnStdout: true,
-          script: 'su - vagrant'
-             script: 'ansible-playbook -u vagrant /var/lib/jenkins/workspace/MyPipeline8-from-git/https-nginx-proxy-tomcat.yml'
+       withCredentials([sshUserPrivateKey(credentialsId: 'jenkins-key',
+                                        keyFileVariable: '',
+                                     passphraseVariable: '',
+                                       usernameVariable: '')]) {
+       sh label: '',
+   returnStdout: true,
+         script: 'ansible-playbook -u vagrant /var/lib/jenkins/workspace/MyPipeline8-from-git/https-nginx-proxy-tomcat.yml'
 
-    }
+     }
    }  
 }
