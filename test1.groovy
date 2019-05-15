436da59b00757ac37483f3b7f2988e71a591a48f
GeorgiDImitrov <46896099+GeorgiDimitrov19@users.noreply.github.com>
2019-05-15 10:26:57 +0300

    Update test1.groovy

--- test1.groovy
+++ test1.groovy
@@ -2,6 +2,6 @@ node {
    stage('Build') {
      sh label: '',
         returnStdout: true,
-        script: 'ansible-playbook -u vagrant ansible_tests/test1.yml'
+        script: 'cd | ansible-playbook -u vagrant ansible_tests/test1.yml'
    }
 }
